b08058e34ee9e2cd7e8ed1ca1eb5957d53382944
karl-william <55711664+karl-william@users.noreply.github.com>
2024-12-09 10:21:11 +0000

    Update +page.svelte

--- src/routes/om/+page.svelte
+++ src/routes/om/+page.svelte
@@ -1,20 +1,10 @@
 <div>
 <img src="English.png" alt="Alternative text">
-<h1>Facts about habitats:</h1>
-<p>
-Habitats: forest, mountain, prairie, arctic, desert & savanna, jungle
-	Forest: Knucker Dragon
-		Lair or Nest: A deep pond, well or ‘knucker hole’
-		Diet: Rabbits, deer, farm animals, stray children
-	Arctic: Frost Dragon
-		Lair or Nest: A sea-facing cave hollowed out from a glacier or iceberg
-		Diet: Giant squid, polar bear, orca, walrus, leopard seal
-	Mountain: Asian Lung Dragon
-		Lair or Nest: Usually an underwater cave or grotto
-		Diet: Mainly fish and birds, particularly roasted swan
+<h1>Facts about habitats:</h1> 
+<h2>Habitats: forest, mountain, prairie, arctic, desert &amp; savanna, jungle</h2>  
+<p>Forest: Knucker Dragon  Lair or Nest: A deep pond, well or &lsquo;knucker hole&rsquo;  Diet: Rabbits, deer, farm animals, stray children  Arctic: Frost Dragon  Lair or Nest: A sea-facing cave hollowed out from a glacier or iceberg  Diet: Giant squid, polar bear, orca, walrus, leopard seal  Mountain: Asian Lung Dragon  Lair or Nest: Usually an underwater cave or grotto  Diet: Mainly fish and birds, particularly roasted swan</p>
 
-Diet: Digestion is fairly straightforwards in dragons. In general, a dragon will eat its prey whole where this is practicable. If not, it may rip it into tasty chucnks that are small enough to eat. A dragon feeds once every few weeks.
+<p>Diet: Digestion is fairly straightforwards in dragons. In general, a dragon will eat its prey whole where this is practicable. If not, it may rip it into tasty chucnks that are small enough to eat. A dragon feeds once every few weeks.</p>
 
-Old or weak dragons who cannot hunt their usual prey may eat men or maidens. This is because maidens wear jewellry, which dragons like to hoard.
-</p>
+<p>Old or weak dragons who cannot hunt their usual prey may eat men or maidens. This is because maidens wear jewellry, which dragons like to hoard.</p>
 </div>
